74f1186db6c071d986abde79952274ad03ced290
Emre Turan <itsemreturan@gmail.com>
2023-05-01 20:20:59 +0300

    fixed some build time issues

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ interface HomeProps {
   searchParams: IListingsParams;
 }
 
+export const dynamic = "force-dynamic";
+
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
