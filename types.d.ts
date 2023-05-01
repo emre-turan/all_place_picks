933c4a100c01cf48390dd6f05c7f61a2d6d3204d
Emre Turan <itsemreturan@gmail.com>
2023-05-01 16:12:19 +0300

    fix

--- types.d.ts
+++ types.d.ts
@@ -4,6 +4,7 @@ import "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: string; // Add this line
       role: string;
       name?: string | null;
       email?: string | null;
